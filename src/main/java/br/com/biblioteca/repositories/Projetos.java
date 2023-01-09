b66143b1e3022a36a8d28153701a88f4a8d4e1fa
Gabriel Mendes <gabrielmendes@Gabriels-MacBook-Air.local>
2023-01-08 23:18:33 -0300

    feat: add schema.sql to create tables

--- src/main/java/br/com/biblioteca/repositories/Projetos.java
+++
@@ -1,9 +0,0 @@
-package br.com.biblioteca.repositories;
-
-import br.com.biblioteca.models.Pessoa;
-import br.com.biblioteca.models.Projeto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface Projetos extends JpaRepository<Projeto, Long> {
-}
